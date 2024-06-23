9e2a849519667d4d2057267030e6b10d80f5f7de
marabiocho <103324080+marabiocho@users.noreply.github.com>
2024-06-23 14:23:55 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,5 @@
 provider "aws" {
-  region = var.region
+  region     = "us-east-1"  # Replace with your AWS region
+  access_key = "AKIATMSVJXSAZIOX6TGG"
+  secret_key = "XMe9c3u2FYt1u0JzrmJRGmuaEOnOblIqpAFo+F7x"
 }
